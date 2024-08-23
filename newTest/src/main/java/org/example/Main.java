8ca84621ff8aa69da534258639e6548e04c1125e
danielpat1209 <158045479+danielpat1209@users.noreply.github.com>
2024-08-23 14:36:18 +0300

    Add files via upload

---
+++ newTest/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import utils.Utils;
+
+import java.awt.*;
+import java.net.URI;
+
+public class Main {
+
+
+    //Google drive link: https://docs.google.com/document/d/16XKREqqzlJNLWHq5jZIminFVNuhuBTZHqwBKAsZnDFw/edit?usp=sharing
+
+    public static void main(String[] args) {
+
+        // Utils.saveIdentifyCode();
+        if (true) {
+            System.out.println(Utils.getCreds());
+            Window window = new Window();
+
+        }
+
+    }
+}
